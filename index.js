0bed6bef6745c416ed3a953ac24308afe14ef69e
lgrave264 <71471845+lgrave264@users.noreply.github.com>
2021-11-10 15:03:34 -0700

    added a search parameter by season and episode, still need description and make version for characters

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const express = require('express')
 const app = express()
-const { season1, season2, season3, season4, characters } = require('./data')
+const { seasons, characters } = require('./data')
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/season1">season1</a>')
+    res.send('<h1>Home Page</h1><a href="/api/seasons">seasons</a>')
 })
-app.get('/api/season1', (req, res) => {
-    const newSeason1 = season1.map((product) => {
+app.get('/api/seasons', (req, res) => {
+    const newSeasons = seasons.map((product) => {
         const { id, title, place, image, desc } = product
         return { id, title, place, image, desc }
     })
 
-    res.json(newSeason1)
+    res.json(newSeasons)
 })
-app.get('/api/season1/:season1ID', (req, res) => {
+app.get('/api/seasons/:seasonsID', (req, res) => {
     //console.log(req)
     //console.log(req.params)
-    const { season1ID } = req.params
+    const { seasonsID } = req.params
 
-    const singleEpisode = season1.find(
-        (season1) => season1.id === Number(season1ID)
+    const singleEpisode = seasons.find(
+        (seasons) => seasons.id === Number(seasonsID)
+    )
+    if(!singleEpisode){
+        return res.status(404).send('Product Does Not Exist')
+    }
+
+    return res.json(singleEpisode)
+})
+
+app.get('/api/characters', (req, res) => {
+    const newCharacter = characters.map((product) => {
+        const { id, title, place, image, desc } = product
+        return { id, title, place, image, desc }
+    })
+
+    res.json(newCharacter)
+})
+app.get('/api/characters/:characterID', (req, res) => {
+    //console.log(req)
+    //console.log(req.params)
+    const { characterID } = req.params
+
+    const singleEpisode = characters.find(
+        (characters) => characters.id === Number(characterID)
     )
     if(!singleEpisode){
         return res.status(404).send('Product Does Not Exist')
@@ -33,21 +56,21 @@ app.get('/api/v1/query', (req,res) => {
     //(query?search=fight)
     //(query?search=fight&limit=3)
     const { search, limit } = req.query
-    let sortedSeason1 = [...season1]
+    let sortedSeasons = [...seasons]
 
     if(search){
-        sortedSeason1 = sortedSeason1.filter((season1) => {
-            return season1.desc.includes(search)
+        sortedSeasons = sortedSeasons.filter((seasons) => {
+            return seasons.place.includes(search)
         })
     }
     if(limit){
-        sortedSeason1 = sortedSeason1.slice(0 , Number(limit))
+        sortedSeasons = sortedSeasons.slice(0 , Number(limit))
     }
-    if(sortedSeason1.length < 1){
+    if(sortedSeasons.length < 1){
         //res.status(200).send('no products matched your search');
         return res.status(200).json({sucess: true, data: [] })
     }
-    res.status(200).json(sortedSeason1)
+    res.status(200).json(sortedSeasons)
 })
 
 app.listen(1234, () => {
